b14667468b841fd31172a0a7b72bace9af8864aa
Eliete Campos <lilagomes3@hotmail.com>
2025-11-10 18:24:06 -0300

    deploy e atualizaçao

---
+++ src/app/modules/conponents/knowledge/knowledge.component.ts
@@ -0,0 +1,51 @@
+import { Component, signal } from '@angular/core';
+
+//interface
+import { IKnowledge } from '../../portifolio/interface/Iknowledge.interface';
+
+@Component({
+  selector: 'app-knowledge',
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss',
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/knowledge/html.svg',
+      alt: 'icon de conhecimento de html5',
+    },
+    {
+      src: 'assets/knowledge/angular.svg',
+      alt: 'icon de conhecimento de angular',
+    },
+    {
+      src: 'assets/knowledge/css.svg',
+      alt: 'icon de conhecimento de css',
+    },
+    {
+      src: 'assets/knowledge/js.svg',
+      alt: 'icon de conhecimento de javascript',
+    },
+    {
+      src: 'assets/knowledge/csharp.svg',
+      alt: 'icon de conhecimento de csharp',
+    },
+    {
+      src: 'assets/knowledge/bootstrap.svg',
+      alt: 'icon de conhecimento de bootstrap5',
+    },
+    {
+      src: 'assets/knowledge/net.core.svg',
+      alt: 'icon de conhecimento de net.core mvc',
+    },
+    {
+      src: 'assets/knowledge/git.svg',
+      alt: 'icon de conhecimento de Git',
+    },
+    {
+      src: 'assets/knowledge/github.png',
+      alt: 'icon de conhecimento de Github',
+    },
+  ]);
+}
---
+++ src/app/modules/portifolio/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//components
+import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../../conponents/knowledge/knowledge.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent, KnowledgeComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
